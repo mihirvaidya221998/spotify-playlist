7f7cc4579cb0df39d18b659fdc10ba4fa24a8cea
Mahajan, Pranav <pmahaja@iu.edu>
2023-12-02 14:34:17 -0500

    basic display playlists and users.

--- src/playlists.js
+++ src/playlists.js
@@ -1,9 +1,50 @@
+//firebase imports
+import { db } from './firebase-config';
+import { collection, getDocs, addDoc, doc, setDoc } from 'firebase/firestore';
+
+import React, { useEffect, useState } from 'react';
+import './playlists.css';
+
+
 function Playlists() {
+    const [playlists, setPlaylists] = useState([]);
+    const [users, setUsers] = useState([]);
+  
+    useEffect(() => {
+      const fetchPlaylists = async () => {
+        const querySnapshot = await getDocs(collection(db, "Playlist"));
+        setPlaylists(querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+      };
+  
+      const fetchUsers = async () => {
+        const querySnapshot = await getDocs(collection(db, "Users"));
+        setUsers(querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+      };
+  
+      fetchPlaylists();
+      fetchUsers();
+    }, []);
+  
+    // Render playlists and users
     return (
-    <div>
-        <h1>Playlists Page</h1>
-    </div>
-    );
+        <div className="Playlists">
+          <h1>All data available</h1>
+          <div className="content">
+            <div className="list">
+              <h2>Playlists</h2>
+              {playlists.map(playlist => (
+                <div key={playlist.id}>{playlist.name}</div>
+              ))}
+            </div>
+            <div className="list">
+              <h2>Users</h2>
+              {users.map(user => (
+                <div key={user.id}>{user.user_name}</div>
+              ))}
+            </div>
+          </div>
+        </div>
+      );
   }
   
   export default Playlists;
